12e36ef20abe93002ec8e4ab7f42e1a8d0eb9a81
shu65 <dolphinripple@gmail.com>
2024-09-29 07:38:44 +0900

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -3,155 +3,32 @@
 import * as vscode from 'vscode';
 import { showChatGPTSuggestion } from './commands/showChatGPTSuggestion';
 import { applySuggestion } from './commands/applySuggestion';
+import { deleteSuggestion } from './commands/deleteSuggestion';
+import { editSuggestion } from './commands/editSuggestion';
+import { SuggestionComment } from './SuggestionComment';
 
-let commentId = 1;
-
-class NoteComment implements vscode.Comment {
-	id: number;
-	label: string | undefined;
-	savedBody: string | vscode.MarkdownString; // for the Cancel button
-	constructor(
-		public body: string | vscode.MarkdownString,
-		public mode: vscode.CommentMode,
-		public author: vscode.CommentAuthorInformation,
-		public parent?: vscode.CommentThread,
-		public contextValue?: string
-	) {
-		this.id = ++commentId;
-		this.savedBody = this.body;
-	}
-}
 
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "text-writing-assistant" is now active!');
+
 	// A `CommentController` is able to provide comments for documents.
 	const commentController = vscode.comments.createCommentController('chatgptSuggestion', 'ChatGPT Suggestion');
 	context.subscriptions.push(commentController);
 
-	// A `CommentingRangeProvider` controls where gutter decorations that allow adding comments are shown
-	commentController.commentingRangeProvider = {
-		provideCommentingRanges: (document: vscode.TextDocument, token: vscode.CancellationToken) => {
-			const lineCount = document.lineCount;
-			return [new vscode.Range(0, 0, lineCount - 1, 0)];
-		}
-	};
+	// Register command
+	context.subscriptions.push(
+		vscode.commands.registerCommand('text-writing-assistant.showChatGPTSuggestion', () => showChatGPTSuggestion(commentController)));
 
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.createNote', (reply: vscode.CommentReply) => {
-		const thread = reply.thread;
-		thread.canReply = false;
-		replyNote(reply);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('text-writing-assistant.applySuggestion', (thread: vscode.CommentThread) => {
+			applySuggestion(thread);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.replyNote', (reply: vscode.CommentReply) => {
-		replyNote(reply);
+	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.deleteSuggestion', (thread: vscode.CommentThread) => {
+		deleteSuggestion(thread);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.startDraft', (reply: vscode.CommentReply) => {
-		const thread = reply.thread;
-		thread.contextValue = 'draft';
-		const newComment = new NoteComment(reply.text, vscode.CommentMode.Preview, { name: 'vscode' }, thread);
-		newComment.label = 'pending';
-		thread.comments = [...thread.comments, newComment];
+	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.editSuggestion', (comment: SuggestionComment) => {
+		editSuggestion(comment);
 	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.finishDraft', (reply: vscode.CommentReply) => {
-		const thread = reply.thread;
-
-		if (!thread) {
-			return;
-		}
-
-		thread.contextValue = undefined;
-		thread.collapsibleState = vscode.CommentThreadCollapsibleState.Collapsed;
-		if (reply.text) {
-			const newComment = new NoteComment(reply.text, vscode.CommentMode.Preview, { name: 'vscode' }, thread);
-			thread.comments = [...thread.comments, newComment].map(comment => {
-				comment.label = undefined;
-				return comment;
-			});
-		}
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.deleteNoteComment', (comment: NoteComment) => {
-		const thread = comment.parent;
-		if (!thread) {
-			return;
-		}
-
-		thread.comments = thread.comments.filter(cmt => (cmt as NoteComment).id !== comment.id);
-
-		if (thread.comments.length === 0) {
-			thread.dispose();
-		}
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.deleteNote', (thread: vscode.CommentThread) => {
-		thread.dispose();
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.cancelsaveNote', (comment: NoteComment) => {
-		if (!comment.parent) {
-			return;
-		}
-
-		comment.parent.comments = comment.parent.comments.map(cmt => {
-			if ((cmt as NoteComment).id === comment.id) {
-				cmt.body = (cmt as NoteComment).savedBody;
-				cmt.mode = vscode.CommentMode.Preview;
-			}
-
-			return cmt;
-		});
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.saveNote', (comment: NoteComment) => {
-		if (!comment.parent) {
-			return;
-		}
-
-		comment.parent.comments = comment.parent.comments.map(cmt => {
-			if ((cmt as NoteComment).id === comment.id) {
-				(cmt as NoteComment).savedBody = cmt.body;
-				cmt.mode = vscode.CommentMode.Preview;
-			}
-
-			return cmt;
-		});
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.editNote', (comment: NoteComment) => {
-		if (!comment.parent) {
-			return;
-		}
-
-		comment.parent.comments = comment.parent.comments.map(cmt => {
-			if ((cmt as NoteComment).id === comment.id) {
-				cmt.mode = vscode.CommentMode.Editing;
-			}
-
-			return cmt;
-		});
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('text-writing-assistant.dispose', () => {
-		commentController.dispose();
-	}));
-
-	function replyNote(reply: vscode.CommentReply) {
-		const thread = reply.thread;
-		const newComment = new NoteComment(reply.text, vscode.CommentMode.Preview, { name: 'vscode' }, thread, thread.comments.length ? 'canDelete' : undefined);
-		if (thread.contextValue === 'draft') {
-			newComment.label = 'pending';
-		}
-
-		thread.comments = [...thread.comments, newComment];
-	}
-
-		// Register command
-		context.subscriptions.push(
-			vscode.commands.registerCommand('text-writing-assistant.showChatGPTSuggestion', () => showChatGPTSuggestion(commentController)));
-
-		context.subscriptions.push(
-			vscode.commands.registerCommand('text-writing-assistant.applySuggestion', (thread: vscode.CommentThread) => {
-				applySuggestion(thread);
-		}));
 }
